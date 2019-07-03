89ffd91684390ff53a3aa743f43a239c1a82ecf0
Parinya Onsuwan <parinya.ons@gmail.com>
2019-07-04 00:06:34 +0700

    Add QRCode page

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,3 +1,48 @@
 import React from 'react'
-const Login = () => <div>Login page</div>
+import axios from 'axios'
+class Login extends React.Component {
+	state = {
+		username: '',
+		password: ''
+	}
+
+	handleChangedInput = e => {
+		this.setState({
+			[e.target.name]: e.target.value
+		})
+	}
+
+	handleSubmit = async e => {
+		e.preventDefault()
+		try {
+			const response = await axios.post('http://localhost:4000/api/login', {
+				username: this.state.username,
+				password: this.state.password
+			})
+			localStorage.setItem('token', response.data.token)
+			this.props.history.push('/qrcode')
+		} catch (e) {
+			console.log(e)
+		}
+	}
+
+	render() {
+		return (
+			<div>
+				<form onSubmit={this.handleSubmit}>
+				<div>
+					<label htmlFor="">Username:</label>
+					<input type="text" name="username" onChange={this.handleChangedInput} />
+				</div>
+				<div>
+					<label htmlFor="">Password:</label>
+					<input type="password" name="password" onChange={this.handleChangedInput} />
+				</div>
+				<button type="submit" >Login</button>
+				</form>
+			</div>
+		)
+	}
+}
+
 export default Login
--- src/Components/QRCode.jsx
+++ src/Components/QRCode.jsx
@@ -1,3 +1,45 @@
 import React from 'react'
-const QRCode = () => <div>QR code page</div>
+import axios from 'axios'
+class QRCode extends React.Component {
+	state = {
+		amount: 0,
+		qrcode: null
+	}
+
+	handleAmount = e => {
+		this.setState({ amount: e.target.value })
+	}
+
+	handleSubmit = async e => {
+		e.preventDefault();
+		try {
+			const response = await axios.post('http://localhost:4000/api/scb/qrcode', { order_amount: this.state.amount }, {
+				headers: {
+					Authorization: `Bearer ${localStorage.getItem('token')}`
+				}
+			})
+			this.setState({
+				qrcode: response.data
+			})
+		} catch (e) {
+			console.log(e)
+		}
+	}
+
+	render() {
+		return (
+			<div>
+				<div>
+					<form onSubmit={this.handleSubmit}>
+						<label htmlFor="">Amount: </label>
+						<input type="text" onChange={this.handleAmount} />
+						<button type="submit">Generate</button>
+					</form>
+				</div>
+				{ this.state.qrcode &&<div><img src={`data:image/jpeg;base64,${this.state.qrcode.qrImage}`} alt=""/></div> }
+			</div>
+		)
+	}
+}
+
 export default QRCode
